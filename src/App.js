347238bb0dc6f45f5f3375272879de23c777051e
Raghav Kanwal <raghav.kanwal@gmail.com>
2021-07-11 08:38:22 +0530

    Fetching Pokemon Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import pokemonList from './assets/data/pokemon.json';
+
+const PokemonRow = ((pokemon) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(', ')}</td>
+  </tr>
+))
 
 function App() {
   return (
     <div className="app">
       <h1>Search Pokemon</h1>  
+
+      <table className="pokemon-registry">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            pokemonList.map((pokemon) => (
+              <PokemonRow key={pokemon.id} pokemon={pokemon} />
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
