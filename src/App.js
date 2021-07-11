4aed26d3365637903505686d62523efcc7886816
Raghav Kanwal <raghav.kanwal@gmail.com>
2021-07-11 06:19:39 +0530

    updated text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing out react deployments
         </a>
       </header>
     </div>
